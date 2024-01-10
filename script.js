85a4c6192bc9bd415975c62e4534a643905554f1
Jessica <145158102+JessicaY33@users.noreply.github.com>
2024-01-10 15:00:56 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+const stats = ["Mean", "Median", "Mode"];
+const rounds = ["Round #"];
+const frequency = [];
+const data = [];
+let diceAmount = 0;
+let mean = 0;
+let median = 0;
+let mode = 0;
+
+console.log("active");
+
+//Source: https://stackoverflow.com/questions/8922002/attach-event-listener-through-javascript-to-radio-button
+
+function setUp()
+{
+ //reads the results of the form and will set up the frequency and rounds array accordingly 
+}
+
+//Source: https://www.w3schools.com/JS/js_random.asp
+function randomNum()
+{
+  //returns a random number between 1 to 6
+  return Math.floor(Math.random() * 6) + 1;
+}
+
 //Source: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces#creating_an_html_table_dynamically
-function makeTable(row, column)
+function makeTable(row, column, data)
 {
     const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
 
-    for (let i = 0; i < row; i++) {
+    for (let i = 0; i < row.length; i++) {
         const row = document.createElement("tr");
 
         for (let j = 0; j < column; j++) {
